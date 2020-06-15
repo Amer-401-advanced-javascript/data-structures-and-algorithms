114b3f84d8e80ed3cc2852717ed441d2907925f5
Amer-bit <ny.1995@outlook.com>
2020-06-15 08:48:32 +0300

    notFinishedYet

--- challenges/fizzBuzzTree/fizz-buzz-tree.js
+++ challenges/fizzBuzzTree/fizz-buzz-tree.js
@@ -1,11 +1,20 @@
 'use strict';
 
+let BinarySearchTree = require('../tree/tree');
 
 // eslint-disable-next-line no-unused-vars
 function FizzBuzzTree (tree){
     
+  console.log('hello');
+  let currentNode = tree.root;
+  traverse(currentNode);
+  return fizzbuzz(currentNode.value);
 }
 
+function traverse(node){
+  if(node.left) traverse(node.left);  
+  if(node.right) traverse(node.right);
+}
 
 
 function fizzbuzz (value) {
@@ -16,3 +25,18 @@ function fizzbuzz (value) {
     else if((value%3) && (value%5)) {return value.toString();}
   }
 }
+
+
+let BST = new BinarySearchTree();
+BST.add(100);
+BST.add(500);
+BST.add(95);
+BST.add(33);
+BST.add(15);
+// console.log(BST.root);
+
+console.log(FizzBuzzTree(BST));
+
+
+
+
